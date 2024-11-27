231464ea6a62cce8714ef58a17d274fe71ec6035
Omar Toledo <o.toledo@uniandes.edu.co>
2024-11-27 13:02:14 -0500

    Images split in classes

--- src/MLDS6_Assets/database/__init__.py
+++ src/MLDS6_Assets/database/__init__.py
@@ -2,12 +2,76 @@ import os
 import json
 from kaggle.api.kaggle_api_extended import KaggleApi # type: ignore
 import shutil
+import re
+
+def separateImages(originalPath,targetPath):
+  """
+  Given a target path, separates the images by class in a target directory
+
+  Parameters:
+  ----------
+    originalPath (str): Source path of the images
+    targetPath (str): Desgtiny path to the images separated by classes
+  """
+  pattern = re.compile(r'MLDS2_Project_Data[/\\]([^/\\]+)[/\\]([^/\\]+)$')
+  m = pattern.search(targetPath)
+  s1 , s2 = m.groups()
+
+  mapper_path = {
+      'Normal' : 'NORMAL',
+      'bacterialPneumonia' : 'PNEUMONIA',
+      'virusPneumonia' : 'PNEUMONIA'
+  }
+
+  originPath = originalPath+f'/{s1}/{mapper_path[s2]}'
+
+  mapper = {
+      'bacterialPneumonia' : 'bacteria',
+      'virusPneumonia' : 'virus'
+  }
+
+  if mapper_path[s2]=='NORMAL':
+    for item in os.listdir(originPath):
+        s = os.path.join(originPath, item)
+        d = os.path.join(targetPath, item)
+        shutil.copy2(s, d)
+  else:
+    for item in os.listdir(originPath):
+      if mapper[s2] in item:
+        s = os.path.join(originPath, item)
+        d = os.path.join(targetPath, item)
+        shutil.copy2(s, d)
+  print(f'Copied files to {targetPath}')
+
+def createDirectoryWithSubdirs(base_dir, subdirs):
+    """
+    Given a base dir, creates a set of subdirs inside of it
+
+    Parameters:
+    ----------
+    base_dir (str): Path of the base directory that is wanted to be created
+    subdirs (str): Name of the subdirectories that are wanted to be created inside the base directory
+
+    Returns:
+    ----------
+        List of path names of the created directories (List of str)
+
+    """
+    os.makedirs(base_dir, exist_ok=True)
+    dirs = []
+    for subdir in subdirs:
+        path = os.path.join(base_dir, subdir)
+        os.makedirs(path, exist_ok=True)
+        dirs.append(path)
+        print(f"Directory created: {path}")
+    return dirs
 
 def deleteDirectoryAndContents(directory):
     """
     Deletes a directory and all its contents (files and subdirectories).
 
     Parameters:
+    ----------
     directory (str): The path to the directory to delete.
     """
     try:
